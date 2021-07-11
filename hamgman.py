bfa838fa29022d65fd1540d264c8c62eedfddfd6
jayant287 <67455822+jayant287@users.noreply.github.com>
2021-07-11 14:16:17 +0530

    Create hamgman.py

---
+++ hamgman.py
@@ -0,0 +1,38 @@
+from random import choice
+def random_generate():
+    with open("words.txt","r") as f:
+        lines=f.readlines()
+    f.close()
+    lines=[i.strip("\n") for i in lines]
+    return choice(lines)
+name = input("What is your name? \n")
+print("Hello, " + name, "Lets play hangman!")
+print("Start guessing...")
+word = random_generate()
+guesses = ""
+turns = len(word)+3
+while turns > 0:         
+    fail = 0             
+    for char in word:      
+        if char in guesses:    
+            print (char,end=" ")   
+
+        else:
+            print ("_",end=" ")     
+            fail += 1    
+    if fail == 0:
+        print("\n")        
+        print ("C0ngratulations You  have won !!!!!")
+        print("The word is:",word)
+        break              
+    guess = input("guess a character:") 
+    guesses += guess                    
+    if guess not in word:  
+        turns -= 1
+        print("Wrong")
+        print("You have more "+str(turns)+" turns remaining" ) 
+        if turns == 0:           
+            print("You have lost")
+            print("The word was: ",word)
+            print("Better luck next time")
+ 
